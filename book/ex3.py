89b2b8501de93533f2ee9f91032b7f472d6356d4
Gon <gonzalo.sanchter@gmail.com>
2018-04-02 12:19:36 +0200

    add ex3.py

---
+++ book/ex3.py
@@ -0,0 +1,32 @@
+# Print string
+print("I will now count my chickens:")
+
+# Print String and print a flout. Result of operation 25 + 30 / 6
+print("Hens", 25.0 + 30.0 / 6)
+# Print String and print a float. Result of operation 100 - 25 * 3 % 4
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Print string
+print("Now I will count the eggs:")
+
+# Print a float. Result of operation 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Print a boolean. False. After calculate result of 3 + 2 < 5 - 7 and compare
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("what is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# Print a boolean. True. After compare result of 5 > -2
+print("Is it greater?", 5.0 > -2.0)
+# Print a boolean. True. After compare result of 5 >= -2
+print("Is it greater or equal?", 5.0 >= -2.0)
+# Print a boolean. False. After compare result of 5 <= -2
+print("Is it less or equal?", 5.0 <= -2.0)
