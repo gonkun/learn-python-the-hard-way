4586d8cb0da83fe2e4558a629fc78c1ad439c790
Gon <gonzalo.sanchter@gmail.com>
2018-04-15 01:04:03 +0200

    add ex5.py

---
+++ book/ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Gonzalo Sánchez'
+my_age = 34  # sigh
+my_height = 178  # cm
+my_weight = 70  # kg
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} Kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} har.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# This line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
