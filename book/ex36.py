2f0941e9eb471efa6694c64f482bd8736dea61e9
Gon <gonzalo.sanchter@gmail.com>
2018-08-20 01:03:40 +0200

    add exercise ex36.py. My own game

--- book/ex36.py
+++ book/ex36.py
@@ -23,7 +23,10 @@ def main_room():
             else:
                 print("You entered in this room before. Now it seems closed...")
         elif choice.lower() == "center":
-            monster_room()
+            if "key" not in inventory:
+                monster_room()
+            else:
+                print("You entered in this room before. Now it seems closed...")
         elif choice.lower() == "right":
             treasure_room()
         else:
@@ -43,11 +46,51 @@ def mystery_room():
         print("When you open it you discover that inside there is a sword with runes.")
         print("You have equipped it!")
         inventory.append("sword")
-        print(inventory)
         print("You return to the main room.")
         main_room()
     else:
         dead("The faint light disappears for a moment and suddenly you see how lightning strikes you in the chest...")
+
+
+def monster_room():
+    print("You cross the door on the center ...")
+    print("There is an unbearable stench along with a grunt.")
+    print("You look up and see a huge troll appear from the shadows.")
+    print("The door will not open.")
+    print("Do you attack or flee across the room? (attack/flee)")
+
+    while True:
+        answer = input("> ")
+
+        if answer.lower() == "attack":
+            if "sword" not in inventory:
+                print("They do not have any weapons.")
+                dead("You try to punch the troll in the face. But, as expected, the troll does not flinch and rips your head off.")
+            else:
+                print("You take out the sword and jump straight to the troll.")
+                print("You do a somersault in the air and you put the sword in the neck.")
+                print("The monster collapses, and drops a key.")
+                print("You have equipped it!")
+                inventory.append("key")
+                print("You return to the main room.")
+                main_room()
+        elif answer.lower() == "flee":
+            dead("You try to run away, but the troll reaches you makes a soup with your tips")
+        else:
+            print("Sorry. Can you repeat, please? I don't understand your answer...\n")
+
+
+def treasure_room():
+    print("The room is small but what matters is the center chest.")
+    print("The chest is big and old.")
+    if "key" not in inventory:
+        print("You notice that you do not have the key to open the chest.")
+        print("You decide to leave the room and look for it, but the door does not open.")
+        dead("You are stuck FOREVER...")
+    else:
+        print("When you enter the key and observe the wonderful treasure.\n")
+        print("Congratulations! You are rich!")
+        exit(0)
 
 
 def start():
@@ -57,7 +100,6 @@ def start():
     while True:
         print("Do you want enter inside? (yes/no)")
         choice = input("> ")
-        
         if choice.lower() == "no":
             dead("Oh.. I see... You're another coward. Well, see you soon!")
         elif choice.lower() == "yes":
